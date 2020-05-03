e8a99453b99405d7a1f4cc3db768460d57414924
AdrianSeguraOrtiz <adrianseor.99@uma.es>
2020-05-03 14:27:54 +0200

    Bucle for sustituye recursividad

--- src/main/java/fibonacci/Fibonacci.java
+++ src/main/java/fibonacci/Fibonacci.java
@@ -7,7 +7,17 @@ public class Fibonacci {
         } else if (n == 1 || n == 2) {
             return 1;
         } else {
-            return k * computar(n - 2, k) + computar(n - 1, k);
+            // Computacionalmente es mejor bucle a recursividad
+            //return computar(n - 1, k) + k * computar(n - 2, k);
+            int Fi_2 = 1;
+            int Fi_1 = 1;
+            int Fi = 1;
+            for (int i = 3; i <= n; i++){
+                Fi = Fi_1 + k * Fi_2;
+                Fi_2 = Fi_1;
+                Fi_1 = Fi;
+            }
+            return Fi;
         }
     }
 }
